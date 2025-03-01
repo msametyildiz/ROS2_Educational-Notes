d15c5e1e828bbe46b19b45ac3fb513e72c26a333
msametyildiz <muhammedsamet.yildiz.06@gmail.com>
2025-03-01 17:19:49 +0300

    initial commit

---
+++ src/simple_py_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'simple_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='muhammed-samet-yildiz',
+    maintainer_email='muhammed-samet-yildiz@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "py_node = simple_py_pkg.simple_py_node:main"
+        ],
+    },
+)
